d26fd878765c392df0a7cb145aa24526e4e02df9
Alistair Hassin <alistairh96@outlook.com>
2024-01-11 20:09:49 +0000

    test webhook

--- Templates/adultProfile.php
+++ Templates/adultProfile.php
@@ -19,7 +19,7 @@
                     <a href="{{ url_for('watch_details', video_id=video['_id']) }}">Watch Details</a>
                     <video controls>
                         <source src="{{ video['url'] }}" type="video/mp4">
-                        Your browser does not support the video tag.
+                        Your browser does not support the video .
                     </video>
                 </div>
             {% endfor %}
