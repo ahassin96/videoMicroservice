700066ee661c2fbb3c151faa741e4a64e005000c
Alistair Hassin <alistairh96@outlook.com>
2024-01-12 15:11:31 +0000

    another test 12

--- videoapp.py
+++ videoapp.py
@@ -10,7 +10,7 @@ CORS(app)
 
 
 
-@app.route('/api/video/<video_id>', methods=['GET'])
+@app.route('/hello/<video_id>', methods=['GET'])
 def get_video_details(video_id):
     app.logger.info(f'Received API request for video ID: {video_id}')
     try:
